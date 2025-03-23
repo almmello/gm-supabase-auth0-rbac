83972691a48b4e65175fb452e6098973e24c6f5e
Alexandre Monteiro de Mello <almmello@gmail.com>
2025-03-23 20:24:54 -0300

    ajuste de favicon no dashboard

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -5,8 +5,10 @@ export default function BaseLayout({ children }) {
     <>
       <Head>
         <title>Goalmoon TODO</title>
-        <meta name="description" content="Goalmoon TODO - Gerenciador de tarefas" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Goalmoon TODO - Gerencie suas tarefas com simplicidade e segurança" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="theme-color" content="#293047" />
         {/* Preload da imagem do logo */}
         <link
           rel="preload"
