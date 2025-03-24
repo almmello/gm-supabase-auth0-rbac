ea143c64f41f6b5331739d9b1c45a13eea0dc0f7
Alexandre Monteiro de Mello <almmello@gmail.com>
2025-03-23 22:50:16 -0300

    mudança para jpg

--- pages/_app.js
+++ pages/_app.js
@@ -17,25 +17,24 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" type="image/png" href="/favicon.png" />
         <meta name="theme-color" content="#293047" />
         
-        {/* OpenGraph / Facebook */}
+        {/* OpenGraph Tags */}
         <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Goalmoon TODO" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="https://todo.gmoo.com.br/opengraph-image.png" />
-        <meta property="og:site_name" content="Goalmoon TODO" />
+        <meta property="og:url" content="https://todo.gmoo.com.br" />
         <meta property="og:locale" content="pt_BR" />
-
-        {/* WhatsApp */}
+        <meta property="og:image" content="https://todo.gmoo.com.br/opengraph-image.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Goalmoon TODO - Gerenciador de Tarefas" />
-        <meta property="og:url" content="https://todo.gmoo.com.br" />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="https://todo.gmoo.com.br/opengraph-image.png" />
+        <meta name="twitter:image" content="https://todo.gmoo.com.br/opengraph-image.jpg" />
         
         {/* Preload da imagem do logo */}
         <link
