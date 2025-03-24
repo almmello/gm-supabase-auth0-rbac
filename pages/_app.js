3e636ede42062ad1f227f740d5f30d6e730b2c95
Alexandre Monteiro de Mello <almmello@gmail.com>
2025-03-23 22:33:08 -0300

    Ajuste de domínio para og

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ export default function App({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content="/opengraph-image.png" />
+        <meta property="og:image" content="https://todo.gmoo.com.br/opengraph-image.png" />
         <meta property="og:site_name" content="Goalmoon TODO" />
         <meta property="og:locale" content="pt_BR" />
 
@@ -29,12 +29,13 @@ export default function App({ Component, pageProps }) {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Goalmoon TODO - Gerenciador de Tarefas" />
+        <meta property="og:url" content="https://todo.gmoo.com.br" />
 
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="/opengraph-image.png" />
+        <meta name="twitter:image" content="https://todo.gmoo.com.br/opengraph-image.png" />
         
         {/* Preload da imagem do logo */}
         <link
