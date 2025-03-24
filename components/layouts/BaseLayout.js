d943068d74cb2b10f66d6a09b72f6b630fe99644
Alexandre Monteiro de Mello <almmello@gmail.com>
2025-03-23 21:47:50 -0300

    mudança das tags para o _app

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,45 +1,8 @@
 import Head from 'next/head';
 
 export default function BaseLayout({ children }) {
-  const title = "Goalmoon TODO";
-  const description = "Gerencie suas tarefas com simplicidade e segurança usando Next.js, Supabase e Auth0";
-
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <meta name="theme-color" content="#293047" />
-        
-        {/* OpenGraph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content="/images/og-image.png" />
-        <meta property="og:site_name" content="Goalmoon TODO" />
-        <meta property="og:locale" content="pt_BR" />
-
-        {/* WhatsApp */}
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Goalmoon TODO - Gerenciador de Tarefas" />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content="/images/og-image.png" />
-        
-        {/* Preload da imagem do logo */}
-        <link
-          rel="preload"
-          href="/images/goalmoon-logo.png"
-          as="image"
-          type="image/png"
-        />
-      </Head>
       {children}
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,44 @@ import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
+  const title = "Goalmoon TODO";
+  const description = "Gerencie suas tarefas com simplicidade e segurança usando Next.js, Supabase e Auth0";
+
   return (
     <UserProvider>
       <Head>
-        <title>Goalmoon - Todo List</title>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Goalmoon Todo List Application" />
         <meta name="theme-color" content="#293047" />
+        
+        {/* OpenGraph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:site_name" content="Goalmoon TODO" />
+        <meta property="og:locale" content="pt_BR" />
+
+        {/* WhatsApp */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Goalmoon TODO - Gerenciador de Tarefas" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content="/images/og-image.png" />
+        
+        {/* Preload da imagem do logo */}
+        <link
+          rel="preload"
+          href="/images/goalmoon-logo.png"
+          as="image"
+          type="image/png"
+        />
       </Head>
       <Component {...pageProps} />
     </UserProvider>
